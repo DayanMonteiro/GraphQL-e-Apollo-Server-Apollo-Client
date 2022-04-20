06f53ab48d6685fe4d2efca20e62ebb4c55051ca
DayanMonteiro <dmsmamb@gmail.com>
2022-04-20 15:02:15 -0300

    separando queryes e resolvers

--- 01-GraphQl/src/index.js
+++ 01-GraphQl/src/index.js
@@ -1,43 +1,9 @@
-import { ApolloServer, gql } from 'apollo-server';
+import { ApolloServer } from 'apollo-server';
+import { resolvers, typeDefs } from './graphql/schema';
 
 const server = new ApolloServer({
-  typeDefs: gql`
-    type Query {
-      user: User!
-      users: [User!]!
-    }
-
-    type User {
-      id: ID!
-      userName: String!
-    }
-  `,
-  resolvers: {
-    Query: {
-      user: () => {
-        return {
-          id: 'a1s2d3f487',
-          userName: 'Dayan Monteiro',
-        };
-      },
-      users: () => {
-        return [
-          {
-            id: 'a1s2d3f487',
-            userName: 'Dayan Monteiro',
-          },
-          {
-            id: 'b154e2ff487',
-            userName: 'Laryssa Viana',
-          },
-          {
-            id: 'c154e3gm487',
-            userName: 'Pietra Viana Monteiro',
-          },
-        ];
-      },
-    },
-  },
+  typeDefs,
+  resolvers,
 });
 
 server.listen(4003).then(({ url }) => {
