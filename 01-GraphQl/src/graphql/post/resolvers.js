1539d264c1cb52794879370e22e8141f2aca2be6
DayanMonteiro <dmsmamb@gmail.com>
2022-04-22 16:51:25 -0300

    usando um dataloader

--- 01-GraphQl/src/graphql/post/resolvers.js
+++ 01-GraphQl/src/graphql/post/resolvers.js
@@ -1,23 +1,9 @@
+import DataLoader from 'dataloader';
+import fetch from 'node-fetch';
+
 const post = async (_, { id }, { getPosts }) => {
   const response = await getPosts('/' + id);
   const post = await response.json();
-
-  if (Math.random() > 0.5) {
-    return {
-      statusCode: 500,
-      message: 'Post timeout!',
-      timeout: 123,
-    };
-  }
-
-  if (typeof post.id === 'undefined') {
-    return {
-      statusCode: 404,
-      message: 'Post not found!',
-      postId: id,
-    };
-  }
-
   return post;
 };
 
@@ -27,27 +13,19 @@ const posts = async (_, { input }, { getPosts }) => {
   return response.json();
 };
 
+const userDataLoader = new DataLoader(async (ids) => {
+  const urlQuery = ids.join('&id=');
+  const url = 'http://localhost:3000/users/?id=' + urlQuery;
+  const response = await fetch(url);
+  const users = await response.json();
+  return ids.map((id) => users.find((user) => user.id === id));
+});
+
+const user = async ({ userId }, _, { getUsers }) => {
+  return userDataLoader.load(userId);
+};
+
 export const postResolvers = {
   Query: { post, posts },
-  Post: {
-    unixTimestamp: ({ createdAt }) => {
-      const timestamp = new Date(createdAt).getTime() / 1000;
-      return Math.floor(timestamp);
-    },
-  },
-  PostResult: {
-    __resolveType: (obj) => {
-      if (typeof obj.postId !== 'undefined') return 'PostNotFoundError';
-      if (typeof obj.timeout !== 'undefined') return 'PostTimeoutError';
-      if (typeof obj.id !== 'undefined') return 'Post';
-      return null;
-    },
-  },
-  PostError: {
-    __resolveType: (obj) => {
-      if (typeof obj.postId !== 'undefined') return 'PostNotFoundError';
-      if (typeof obj.timeout !== 'undefined') return 'PostTimeoutError';
-      return null;
-    },
-  },
+  Post: { user },
 };
