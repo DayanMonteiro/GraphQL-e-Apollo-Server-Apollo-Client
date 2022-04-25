e8e48286fbe78ffb5452766aaa65712665856d0d
DayanMonteiro <dmsmamb@gmail.com>
2022-04-25 11:12:05 -0300

    implementando RESTDataSource do Apollo

---
+++ 01-GraphQl/src/graphql/post/datasources.js
@@ -0,0 +1,18 @@
+import { RESTDataSource } from 'apollo-datasource-rest';
+
+export class PostsApi extends RESTDataSource {
+  constructor() {
+    // chama o construtor da classe
+    super();
+    this.baseURL = process.env.API_URL + '/posts/';
+  }
+
+  async getPosts(urlParams = {}) {
+    // '', é o primeiro parametro que é o caminho no caso a API_URL
+    return this.get('', urlParams);
+  }
+
+  async getPost(id) {
+    return this.get(id);
+  }
+}
