394984f3bc880d5c1484a39aa9adb6ebcc4872c5
DayanMonteiro <dmsmamb@gmail.com>
2022-05-09 17:29:51 -0300

    criando crud para users

--- 01-GraphQl/src/graphql/user/typedefs.js
+++ 01-GraphQl/src/graphql/user/typedefs.js
@@ -5,6 +5,11 @@ export const userTypeDefs = gql`
     user(id: ID!): User!
     users(input: ApiFiltersInput): [User!]!
   }
+  extend type Mutation {
+    createUser(data: CreateUserInput!): User!
+    updateUser(userId: ID!, data: UpdateUserInput!): User!
+    deleteUser(userId: ID!): Boolean!
+  }
   type User {
     id: ID!
     firstName: String!
@@ -13,5 +18,17 @@ export const userTypeDefs = gql`
     indexRef: Int!
     createdAt: String!
     posts: [Post!]!
+  }
+
+  input CreateUserInput {
+    firstName: String!
+    lastName: String!
+    userName: String!
+  }
+
+  input UpdateUserInput {
+    firstName: String
+    lastName: String
+    userName: String
   }
 `;
