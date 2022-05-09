10f9dbd6c7aa6b1395fba2dd311568c582692560
DayanMonteiro <dmsmamb@gmail.com>
2022-05-09 12:51:39 -0300

    criando a primeira mutation

--- 01-GraphQl/src/graphql/schema.js
+++ 01-GraphQl/src/graphql/schema.js
@@ -11,11 +11,18 @@ const rootTypeDefs = gql`
   type Query {
     _empty: Boolean
   }
+  type Mutation {
+    _empty: Boolean
+  }
 `;
 
 const rootResolvers = {
   Query: {
     _empty: () => true,
+  },
+
+  Mutation: {
+    _empty: Boolean,
   },
 };
 
